95bfd357300a27e64d3d92b268ace1415e1dd583
dalejandri <86029441+dalejandri@users.noreply.github.com>
2021-07-11 15:00:20 -0500

    Update PyPoll.py
    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -70,11 +70,11 @@ with open(file_to_save, "w") as txt_file:
     
     # Print the winning candidate's results to the terminal.
     winning_candidate_summary = (
-        f"-------------------------\n"
+        f"--------------------------\n"
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"-------------------------\n")
+        f"--------------------------\n")
     print(winning_candidate_summary)
     
     # Save the winning candidate's results to the text file.
