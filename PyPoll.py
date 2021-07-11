c54581c015099321e13067ee20c90f71c61428c7
dalejandri <86029441+dalejandri@users.noreply.github.com>
2021-07-11 15:06:09 -0500

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -1,26 +1,19 @@
 # Add our dependencies.
 import csv
 import os
-
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-
 # Initialize a total vote counter.
 total_votes = 0
-
 # Candidate options and candidate votes.
 candidate_options = []
-
-# Declare the empty dictionary.
 candidate_votes = {}
-
 # Track the winning candidate, vote count, and percentage.
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
-
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -46,9 +39,11 @@ with open(file_to_load) as election_data:
 with open(file_to_save, "w") as txt_file:
     # After opening the file print the final vote count to the terminal.
     election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
         f"-------------------------\n")
     print(election_results, end="")
-    
     # After printing the final vote count to the terminal save the final vote count to the text file.
     txt_file.write(election_results)
     for candidate_name in candidate_votes:
@@ -67,15 +62,13 @@ with open(file_to_save, "w") as txt_file:
             winning_count = votes
             winning_candidate = candidate_name
             winning_percentage = vote_percentage
-    
     # Print the winning candidate's results to the terminal.
     winning_candidate_summary = (
-        f"--------------------------\n"
+        f"-------------------------\n"
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"--------------------------\n")
+        f"-------------------------\n")
     print(winning_candidate_summary)
-    
     # Save the winning candidate's results to the text file.
     txt_file.write(winning_candidate_summary)
